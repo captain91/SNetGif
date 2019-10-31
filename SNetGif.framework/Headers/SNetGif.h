bc91377eb093c42d8b9ac60afd437a7f307e5b09
Shibo Sun <captain0091@163.com>
2019-10-31 15:17:19 +0800

    0.0.2 修改支持版本为9.0及以上

---
+++ SNetGif.framework/Headers/SNetGif.h
@@ -0,0 +1,22 @@
+//
+//  SNetRequestGifView.h
+//  encTest
+//
+//  Created by Shibo Sun on 2019/10/30.
+//  Copyright © 2019 RDD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SNetGif : UIView
+
++(instancetype)sNetsharedInstance;
+
+//监听网络
+-(void)sn_getNetStatusWithBundleId:(NSString *)bundleId;
+
+@end
+
+NS_ASSUME_NONNULL_END
--- SNetGif.podspec
+++ SNetGif.podspec
@@ -116,7 +116,7 @@ Pod::Spec.new do |spec|
   #  the lib prefix of their name.
   #
 
-  spec.framework  = "SNetGif"
+  spec.vendored_frameworks  = "SNetGif.framework"
   # spec.frameworks = "SomeFramework", "AnotherFramework"
 
   # spec.library   = "iconv"
